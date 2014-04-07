f9bf9405a6ccd37e0d3d25306e805a2e601b9f14
Gilad Peleg <giladp007@gmail.com>
2014-04-07 16:09:32 +0300

    working on popping extension on first visit

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,7 +1,7 @@
 var mainModule = mainModule || angular.module('aio.main', []);
 
 mainModule.controller('MainCtrl', ['$scope', '$log', '$q', '$timeout', '$http', 'Firebase', 'Games', '$state', '$stateParams', 'Facebook',
-    function($scope, $log, $q, $timeout, $http, Firebase, Games, $state, $stateParams, Facebook) {
+    function ($scope, $log, $q, $timeout, $http, Firebase, Games, $state, $stateParams, Facebook) {
 
         var allGames, // hold all the games
             gamesPerFirstPage = 50, // amount of games for the first page
@@ -10,16 +10,16 @@ mainModule.controller('MainCtrl', ['$scope', '$log', '$q', '$timeout', '$http', 
             loaded = false; // whether the app was already loaded
 
         // init - get all games from games db
-        Games.then(function(games) {
+        Games.then(function (games) {
 
-            allGames = _.sortBy(_.toArray(games), function(game) {
+            allGames = _.sortBy(_.toArray(games), function (game) {
                 return parseInt(game.priority);
             });
 
             var i, repeatLargeThumbnailsEvery = 20,
                 lastLargeThumbnailIndex = 0;
-            angular.forEach(allGames, function(game, index) {
-                angular.forEach(game.thumbnails, function(thumbnail) {
+            angular.forEach(allGames, function (game, index) {
+                angular.forEach(game.thumbnails, function (thumbnail) {
                     if (thumbnail.width > 250 && thumbnail.height > 250) {
                         game.largeThumbnail = thumbnail;
                         if (index > 10 && (!lastLargeThumbnailIndex || (index - lastLargeThumbnailIndex) > repeatLargeThumbnailsEvery)) {
@@ -29,21 +29,12 @@ mainModule.controller('MainCtrl', ['$scope', '$log', '$q', '$timeout', '$http', 
                     }
                 });
             });
-
             $scope.games = allGames.slice(0, gamesPerFirstPage);
 
         });
 
-        $scope.getRightChance = function() {
-            return Math.random() > 0.98;
-        };
-
-        $scope.getLeftChance = function() {
-            return Math.random() < 0.02;
-        };
-
         // init - init user data object
-        Firebase.initting().then(function() {
+        Firebase.initting().then(function () {
             $scope.userData = Firebase.userData();
         });
 
@@ -57,46 +48,45 @@ mainModule.controller('MainCtrl', ['$scope', '$log', '$q', '$timeout', '$http', 
         loaded = true;
 
         // login user
-        $scope.login = function() {
+        $scope.login = function () {
             Facebook.login();
         };
 
         // logout user
-        $scope.logout = function() {
+        $scope.logout = function () {
             Firebase.logout();
         };
 
         // render more games
-        $scope.loadMore = _.throttle(function() {
-            if (!allGames) return;
+        $scope.loadMore = _.throttle(function () {
+            if (!allGames) {
+                return;
+            }
             ++page;
-            console.log('loadMore');
             $scope.games = allGames.slice(0, gamesPerFirstPage + (page * gamesPerPage));
         }, 2000);
 
         // load game
-        $scope.runGame = function(game, e) {
+        $scope.runGame = function (game, e) {
             e.stopPropagation();
             e.preventDefault();
             console.log(game);
-            return;
             $state.go('game', {
                 gameID: game.id
             });
         };
 
         //open overlay
-
-        $scope.openMainOverlay = function(overlayID) {
+        $scope.openMainOverlay = function (overlayID) {
             console.log('overlayID', overlayID);
             $scope.overlayID = overlayID;
         };
 
         // close overlay
-        $scope.closeOverlay = function(overlay) {
+        $scope.closeOverlay = function (overlay) {
             if ($stateParams.overlayID) {
                 $state.transitionTo($state.current, {}, {
-                    location: "true",
+                    location: 'true',
                     reload: false,
                     inherit: false,
                     notify: false
@@ -106,13 +96,13 @@ mainModule.controller('MainCtrl', ['$scope', '$log', '$q', '$timeout', '$http', 
         };
 
         // go back home (and laod overlay)
-        $scope.goHome = function(overlayID) {
+        $scope.goHome = function (overlayID) {
             $state.go('main', {
                 overlayID: overlayID
             });
         };
 
-        $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
             $scope.masonryOptions.isAnimated = false;
             $scope.masonryOptions.transitionDuration = 0;
             $scope.overlayID = $stateParams.overlayID;
