526a0dfc1aace3586d337eed26f6755244ceece0
Shlomi Toussia-Cohen <shlomitc@gmail.com>
2014-04-13 21:37:18 +0300

    everything in place (hopefully), now to make it look like mojo-games

--- src/js/main/games.js
+++ src/js/main/games.js
@@ -151,8 +151,13 @@ gamesModule.service('Games', ['$log', '$q', '$timeout', '$http', 'Firebase',
         };
 
         $scope.getGameZoom = function (game) {
-            var widthFactor = Math.min(1, 640 / game.width);
-            var heightFactor = Math.min(1, 480 / game.height);
+            var widthFactor=1,heightFactor=1;
+            if(game.width>640){
+                widthFactor = Math.min(1, 640 / game.width);
+            }
+            if(game.height>480) {
+                heightFactor = Math.min(1, 480 / game.height);
+            }
             return Math.min(widthFactor,heightFactor);
         };
 
