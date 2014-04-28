b06bedf58a27416cff8f56c07c86d0a88e852d27
Gilad Peleg <giladp007@gmail.com>
2014-04-28 19:27:10 +0300

    Refactor all config files per domain

--- src/js/config/configModule.js
+++ src/js/config/configModule.js
@@ -1,9 +1,42 @@
 var configModule = configModule || angular.module('aio.config', []);
 
 configModule.factory('Config', function () {
-    // var REALM = 'http://play.gamestab.me';
-    var REALM = 'http://www.mojo-games.com';
-    var APP_NAME = 'Mojo Games';
+
+    var getDocInfo = function () {
+        //TODO - extract to external helper
+        var _appname;
+        var _realm;
+        var _chromeId;
+        var _fbId;
+
+        try {
+            if (document.location.hostname === 'play.gamestab.me') {
+                _appname = 'Gamestab';
+                _chromeId = 'amlhfkalaoikfbpoolhpdhignhjhlhko'; // gamestab,
+                _fbId = '1481519478732760'; //Play Gamestab
+            }
+            _realm = document.location.origin;
+        } catch (e) {
+            console.info('Error with doc.location', e);
+        }
+        _appname = _appname || 'Mojo Games';
+        _realm = _realm || 'http://www.mojo-games.com';
+        _chromeId = _chromeId || 'fmpeljkajhongibcmcnigfcjcgaopfid'; //mojo-games
+        _fbId = _fbId || '224435141079794'; //mojo-games
+
+        return {
+            appName: _appname,
+            realm: _realm,
+            chromeId: _chromeId,
+            fbId: _fbId
+        };
+    };
+
+    var _docInfo = getDocInfo();
+    var APP_NAME = _docInfo.appName;
+    var REALM = _docInfo.realm;
+    var FACEBOOK_ID = _docInfo.fbId;
+    var CHROME_ID = _docInfo.chromeId;
 
     var isLocalStorage = (function () {
         try {
@@ -51,9 +84,7 @@ configModule.factory('Config', function () {
         GAMES_PER_FIRSTPAGE: 50, // amount of games for the first page
         GAMES_PER_PAGE: 50, // amount of games for load more games
         POINTS: POINTS,
-        // CHROME_APP_ID: 'heplncibihkggagnaaoigdhkgjmmllme', // bizigames,
-        // CHROME_APP_ID: 'amlhfkalaoikfbpoolhpdhignhjhlhko', // gamestab,
-        CHROME_APP_ID: 'fmpeljkajhongibcmcnigfcjcgaopfid', // mojo games
+        CHROME_APP_ID: CHROME_ID,
         IS_CHROME: (typeof chrome !== 'undefined' && chrome.webstore),
         RETURN_USER: returnUser
     };
@@ -70,8 +101,7 @@ configModule.factory('Config', function () {
 
     var productionConfig = function () {
         return angular.extend(baseConfig, {
-            FACEBOOK_APP_ID: '224435141079794', // mojo games
-            // FACEBOOK_APP_ID: '1481519478732760', //Play Gamestab
+            FACEBOOK_APP_ID: FACEBOOK_ID,
             INVITE_FRIENDS_POST: INVITE_FRIENDS_POST()
         });
     };
