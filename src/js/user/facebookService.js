a2a7f5e23c764c5cfe41a8972e01cfc64ece74aa
Shlomi Toussia-Cohen <shlomitc@gmail.com>
2014-04-28 18:01:13 +0300

    load fb script asynchronously. allegedly will improve out rating at pageSpeed Insights

--- src/js/user/facebookService.js
+++ src/js/user/facebookService.js
@@ -3,24 +3,28 @@ var facebookModule = facebookModule || angular.module('aio.facebook', []);
 
 facebookModule.factory('Facebook', ['$rootScope', '$log', '$q', '$timeout', '$http', 'Firebase', 'Config',
     function ($rootScope, $log, $q, $timeout, $http, Firebase, Config) {
-        FB.init({
-            appId: Config.FACEBOOK_APP_ID,
-            status: false, // check login status
-            cookie: true, // enable cookies to allow the server to access the session
-            xfbml: false // parse XFBML
-        });
-        FB.Event.subscribe('auth.authResponseChange', Firebase.handleFBAuth);
 
+        $.getScript('//connect.facebook.net/en_US/all.js', function () {
+
+            FB.init({
+                appId : Config.FACEBOOK_APP_ID,
+                status: false, // check login status
+                cookie: true, // enable cookies to allow the server to access the session
+                xfbml : false // parse XFBML
+            });
+            FB.Event.subscribe('auth.authResponseChange', Firebase.handleFBAuth);
+
+        });
         return {
             inviteFriends: function () {
                 var defer = $q.defer();
                 FB.ui({
-                        method: 'feed',
-                        name: Config.INVITE_FRIENDS_POST.NAME,
-                        caption: Config.INVITE_FRIENDS_POST.CAPTION,
+                        method     : 'feed',
+                        name       : Config.INVITE_FRIENDS_POST.NAME,
+                        caption    : Config.INVITE_FRIENDS_POST.CAPTION,
                         description: Config.INVITE_FRIENDS_POST.DESCRIPTION,
-                        link: Config.INVITE_FRIENDS_POST.LINK,
-                        picture: Config.INVITE_FRIENDS_POST.PICTURE
+                        link       : Config.INVITE_FRIENDS_POST.LINK,
+                        picture    : Config.INVITE_FRIENDS_POST.PICTURE
                     },
                     function (response) {
                         $rootScope.$apply(function () {
@@ -35,7 +39,7 @@ facebookModule.factory('Facebook', ['$rootScope', '$log', '$q', '$timeout', '$ht
 
                 return defer.promise;
             },
-            login: function () {
+            login        : function () {
                 var defer = $q.defer();
                 FB.login(function (response) {
                     $rootScope.$apply(function () {
